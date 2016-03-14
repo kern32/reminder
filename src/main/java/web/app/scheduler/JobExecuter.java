14455e29ab3d08f09cde20a56923c06ad2b73a8f
kern32 <sergii.rubinov@gmail.com>
2016-03-15 01:23:20 +0200

    change reminder status if it is not sent

--- src/main/java/web/app/scheduler/JobExecuter.java
+++ src/main/java/web/app/scheduler/JobExecuter.java
@@ -6,54 +6,51 @@ import org.quartz.JobDataMap;
 import org.quartz.JobDetail;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import web.app.configuration.DeliveryType;
 import web.app.configuration.StatusType;
 import web.app.entities.Reminder;
-import web.app.entities.Users;
 import web.app.sender.EmailSender;
+import web.app.sender.PhoneSender;
+import web.app.sender.SkypeSender;
 import web.app.service.ReminderService;
-//import web.app.sender.Skype4JavaSender;
-//import web.app.sender.XmppManager;
 
 public class JobExecuter implements Job {
-	private static final Logger LOG = Logger.getLogger(JobExecuter.class);
-	private static EmailSender emailAgent = new EmailSender();
-	//private static Skype4JavaSender skypeAgent = new Skype4JavaSender();
-
-	@Autowired
-	private static ReminderService reminderService;
+	private static Logger log = Logger.getLogger("file");
 
 	public void execute(JobExecutionContext context) throws JobExecutionException {
-		LOG.info("calling remind executer...");
+		log.info("JobExecuter: calling remind job executer");
 
 		JobDetail messageDetail = context.getJobDetail();
 		JobDataMap jobDataMap = messageDetail.getJobDataMap();
 
 		Reminder reminder = (Reminder) jobDataMap.get("reminder");
-		Users user = (Users) jobDataMap.get("user");
+		ReminderService reminderService = (ReminderService) jobDataMap.get("reminderService");
+		DeliveryType deliveryType = reminder.getDelivery();
 
-		String name = user.getName();
-		String message = reminder.getMessage();
-		String receiver = reminder.getReceiver();
-		String deliveryType = reminder.getDelivery();
-
-		if (DeliveryType.EMAIL.equals(deliveryType)) {
-			if (emailAgent.remind(name, receiver, message)) {
-				LOG.info("EMAIL sent...");
-			}
-		/*} else if (DeliveryType.SKYPE.equals(deliveryType)) {
-			if (skypeAgent.remind(name, receiver, message)) {
-				LOG.info("SKYPE sent...");
-			}
-		} else if (DeliveryType.PHONE.equals(deliveryType)) {
-			if (jabberAgent.remind(name, receiver, message)) {
-				System.out.println("SEND SMS");
-			}*/
+		boolean isSent = false;
+		switch(deliveryType){
+			case Email:
+				EmailSender emailAgent = new EmailSender();
+				isSent = emailAgent.send(reminder);
+				break;
+			case Skype:
+				SkypeSender skypeAgent = new SkypeSender();
+				isSent = skypeAgent.send(reminder);
+				break;
+			case Phone:
+				PhoneSender phoneAgent = new PhoneSender();
+				isSent = phoneAgent.send(reminder);
+				break;
 		}
-		reminder.setStatus(String.valueOf(StatusType.DONE));
+		
+		if (isSent){
+			reminder.setStatus(StatusType.DONE);
+			log.info("JobExecuter: change reminder status as done");
+		} else {
+			reminder.setStatus(StatusType.CANCELED);
+			log.info("JobExecuter: change reminder status as canceled");
+		}
 		reminderService.updateReminder(reminder);
 	}
-
 }
