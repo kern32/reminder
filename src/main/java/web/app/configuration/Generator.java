6fbd261dc3762b38e86a80ff209b606491a79780
kern32 <sergii.rubinov@gmail.com>
2016-03-15 01:01:23 +0200

    password generation

--- src/main/java/web/app/configuration/Generator.java
+++ src/main/java/web/app/configuration/Generator.java
@@ -1,21 +1,15 @@
 package web.app.configuration;
 
-import java.io.UnsupportedEncodingException;
-import java.security.GeneralSecurityException;
-
 import org.apache.log4j.Logger;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import web.app.controllers.UserController;
-
 public class Generator {
 
-	private static final Logger LOG = Logger.getLogger(UserController.class);
+	private static Logger log = Logger.getLogger("file");
 
-	public static String encryptPass(String password) throws GeneralSecurityException, UnsupportedEncodingException {
-		LOG.info("encrypting password........");
+	public static String encryptPass(String password) {
+		log.info("Generator: encrypting password");
 		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-		String hashedPassword = passwordEncoder.encode(password);
-		return hashedPassword;
+		return passwordEncoder.encode(password);
 	}
 }
