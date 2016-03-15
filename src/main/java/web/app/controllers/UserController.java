98dc43f83d460c0928e592403eb74a53344a64ff
kern32 <sergii.rubinov@gmail.com>
2016-03-16 01:41:38 +0200

    send comment message implementation

--- src/main/java/web/app/controllers/UserController.java
+++ src/main/java/web/app/controllers/UserController.java
@@ -1,180 +1,180 @@
 package web.app.controllers;
 
-import java.util.UUID;
+import java.io.IOException;
+import java.util.HashMap;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-/*import org.springframework.security.authentication.AuthenticationServiceException;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.core.context.SecurityContextHolder;*/
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import web.app.configuration.Generator;
 import web.app.configuration.RoleType;
 import web.app.entities.UserRole;
 import web.app.entities.Users;
-import web.app.service.ReminderService;
+import web.app.sender.EmailSender;
+import web.app.sender.SkypeSender;
 import web.app.service.UserRoleService;
 import web.app.service.UserService;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.skype.SkypeException;
+
 @Controller
-@RequestMapping(value = "/app/")
 public class UserController {
-	private static final Logger LOG = Logger.getLogger(UserController.class);
-	static Users user;
+	private static Logger log = Logger.getLogger("file");
 
 	@Autowired
 	private UserService userService;
 
 	@Autowired
-	private ReminderService reminderService;
-	
-	@Autowired
 	private UserRoleService userRoleService;
 
-	@RequestMapping(value = "/username/validation.html**", method = RequestMethod.GET)
-	public @ResponseBody String processAJAXUsernameValidation(
-			@RequestParam String text) {
-			String existing_user= "user exists";
-			// Process the request
-			// Prepare the response string
-			return existing_user;
-		}
-	
-	@RequestMapping(value = "/email/validation.html**", method = RequestMethod.GET)
-	public @ResponseBody String processAJAXEmailValidation(
-			@RequestParam String text) {
-			String existing_email = "email exists";
-			// Process the request
-			// Prepare the response string
-			return existing_email;
-		}
-	
-	@RequestMapping({ "/index.html", "/", "" })
-	public ModelAndView index(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		/*LOG.info("going to index.html");
-		String userName = SecurityContextHolder.getContext().getAuthentication().getName();
-		user = userService.findByName(userName);*/
-		ModelAndView view = new ModelAndView("/pages/index");
-		/*request.getSession().setAttribute("count", userService.countReminders(user.getName()));
-		request.getSession().setAttribute("user", user);*/
-		return view;
-	}
-
-	@RequestMapping(value = "/messages.html", method = RequestMethod.GET)
-	public ModelAndView messages(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		LOG.info("login page - GET request");
-		return new ModelAndView("pages/messages");
-	}
-
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
-	public ModelAndView login(@RequestParam(value = "error", required = false) String error,
-			@RequestParam(value = "logout", required = false) String logout) {
-
+	public ModelAndView authorize(@RequestParam(value = "error", required = false) String error, @RequestParam(value = "logout", required = false) String logout) {
+		log.info("UserController: showing login form");
 		ModelAndView model = new ModelAndView();
 		if (error != null) {
-			model.addObject("error", "Invalid username and password!");
+			model.addObject("error", "Invalid username or password!");
 		}
-
 		if (logout != null) {
 			model.addObject("msg", "You've been logged out successfully.");
 		}
 		model.setViewName("pages/login");
-
 		return model;
-
 	}
-
-
-	@RequestMapping(value = "/registration.html", method = RequestMethod.GET)
-	public ModelAndView getRegistration(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		LOG.info("get request registration");
-		return new ModelAndView("pages/registration");
-	}
-
-	@RequestMapping(value = "/registration.html", method = RequestMethod.POST)
-	public ModelAndView postRegistration(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		String name = request.getParameter("req_name");
-		Users user = userService.findByName(name);
-		if (user != null) {
-			LOG.info("user with this email is already exists going to registration.html");
-			request.setAttribute("exists", true);
-			return getRegistration(request, response);
+	
+	@RequestMapping({ "/index", "/", "" })
+	public String showMainPage(ModelMap model) throws Exception {
+		log.info("UserController: showing index.html page");
+		Users user = userService.getActiveUser();
+		if (user == null){
+			return "redirect:/login";
 		}
-		String email = request.getParameter("req_email");
-		String phone = request.getParameter("req_phone").replaceAll("\\D", "");
-		String skype = request.getParameter("req_skype");
-		String pass = request.getParameter("req_psw");
-		String encryptPsw = Generator.encryptPass(pass);
-		// Mailer.sendMail(name, email, pass);
-		user = new Users();
-		user.setName(name);
-		user.setEmail(email);
-		user.setPhone(phone);
-		user.setSkype(skype);
-		user.setPsw(encryptPsw);
-		userService.saveUser(user);	
-		userRoleService.setRole(new UserRole(user.getId(), RoleType.ROLE_USER.getType()));
+		model.addAttribute("user", user);
+		return "/pages/index";
+	}
+	
+	@RequestMapping(value = {"/username/validation", "/email/validation"}, method = RequestMethod.POST, 
+			consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public void processAJAXUserValidation(@RequestBody String data, HttpServletRequest request, HttpServletResponse response) throws IOException {
+		log.info("UserController: check if username or email exists in database");
+		JsonObject json = new JsonParser().parse(data).getAsJsonObject();
+        String param = json.get("param").getAsString();
 		
-		LOG.info("user created and saved, going to index.html");
-		return index(request, response);
+		boolean isExists = false;
+		String servletPath = request.getServletPath();
+		if(servletPath.contains("username")){
+			isExists = userService.findByName(param) != null ? true : false;
+		} else {
+			isExists = userService.findByEmail(param) != null ? true : false;
+		}
+		
+		HashMap<String, Object> JSONROOT = new HashMap<String, Object>();
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		response.setContentType("json");
+		JSONROOT.put("Result", "OK");
+		JSONROOT.put("result", isExists);
+		String jsonArray = gson.toJson(JSONROOT);
+		
+		log.info(jsonArray);
+		response.getWriter().print(jsonArray);
+	}
+	
+	@RequestMapping(value = {"/skype/isSkypeContactAdded"}, method = RequestMethod.POST, 
+			consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public void processAJAXSkypeValidation(@RequestBody String data, HttpServletRequest request, HttpServletResponse response) throws IOException, SkypeException {
+		log.info("UserController: check if user is in skype contact list");
+        JsonObject json = new JsonParser().parse(data).getAsJsonObject();
+        String skypeName = json.get("skypeName").getAsString();
+		
+		String message = null;
+		if (!SkypeSender.isContactAdded(skypeName)) {
+			SkypeSender.addContact(skypeName);
+			message = "Please, add reminder.agent to your skype contact list";
+		}
+		
+		HashMap<String, Object> JSONROOT = new HashMap<String, Object>();
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		response.setContentType("json");
+		JSONROOT.put("Result", "OK");
+		JSONROOT.put("result", message);
+		String jsonArray = gson.toJson(JSONROOT);
+		
+		log.info(jsonArray);
+		response.getWriter().print(jsonArray);
+	}
+	
+	@RequestMapping(value = "/registration", method = RequestMethod.GET)
+	public String showRegistrationPage() throws Exception {
+		log.info("UserController: showing registration form");
+		return "/pages/registration";
+	}
+	
+	@RequestMapping(value = "/registration", method = RequestMethod.POST)
+	public String createUser(@ModelAttribute("userAttribute") Users user, BindingResult bindingResult) throws Exception {
+		log.info("UserController: request to register new user");
+		user.setEnabled(true);
+		user.setPassword(Generator.encryptPass(user.getPassword()));
+		userService.addUser(user);
+		EmailSender emailAgent = new EmailSender();
+		emailAgent.registration(user);
+		userRoleService.setRole(new UserRole(user.getId(), RoleType.ROLE_USER.getType()));
+		log.info("UserController: user created and saved, going to login page");
+		return "redirect:/login";
 	}
 
-	@RequestMapping(value = "/profile.html", method = RequestMethod.GET)
-	public ModelAndView getProfile(HttpServletRequest req, HttpServletResponse res) throws Exception {
-		LOG.info("going to profile.html");
+	@RequestMapping(value = "/profile", method = RequestMethod.GET)
+	public ModelAndView getUserProfile(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		log.info("UserController: showing profile page");
+		Users user = userService.getActiveUser();
 		ModelAndView view = new ModelAndView("pages/profile");
-		/*view.addObject("username", user.getName());
+		view.addObject("id", user.getId());
+		view.addObject("username", user.getUsername());
 		view.addObject("email", user.getEmail());
 		view.addObject("skype", user.getSkype());
 		view.addObject("phone", user.getPhone().replace(" ", ""));
-		view.addObject("pass", Generator.decrypt(user.getPsw(), user.getKey()));*/
+		view.addObject("role", user.getRole());
+		view.addObject("enabled", user.isEnabled());
+		view.addObject("pass", "");
 		return view;
 	}
 
-	@RequestMapping(value = "/profile.html", method = RequestMethod.POST)
-	public ModelAndView postProfile(HttpServletRequest req, HttpServletResponse res) throws Exception {
-		LOG.info("saving profile.html");
-		user.setEmail(req.getParameter("req_email"));
-		user.setPhone(req.getParameter("req_phone"));
-		user.setSkype(req.getParameter("req_skype"));
-		String key = UUID.randomUUID().toString().replaceAll("-", "");
-		user.setPsw(Generator.encryptPass(req.getParameter("req_psw")));
+	@RequestMapping(value = "/profile", method = RequestMethod.POST)
+	public ModelAndView updateUserProfile(@ModelAttribute("userAttribute") Users user, BindingResult bindingResult,
+			HttpServletRequest req, HttpServletResponse res) throws Exception {
+		log.info("UserController: saving user profile");
+		user.setPassword(Generator.encryptPass(user.getPassword()));
 		userService.updateUser(user);
-		return getProfile(req, res);
+		return getUserProfile(req, res);
 	}
 
-	@RequestMapping("/404.html")
-	public ModelAndView pageNotFound(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		LOG.info("going to 404.html");
-		return new ModelAndView("/pages/errors/404");
-	}
-
-	@RequestMapping("/401.html")
-	public ModelAndView unauthorized(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		LOG.info("going to 401.html");
-		return new ModelAndView("/pages/errors/401");
-	}
-
-	@RequestMapping("/403.html")
-	public ModelAndView forbidden(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		LOG.info("going to 403.html");
-		return new ModelAndView("/pages/errors/403");
+	@RequestMapping(value = "/contact.html", method = RequestMethod.GET)
+	public String showContactPage(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		log.info("UserController: showing contact page");
+		return "/pages/contact";
 	}
 	
-	@RequestMapping("/contact.html")
-	public ModelAndView contact(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		LOG.info("going to contact.html");
-		return new ModelAndView("/pages/contact");
+	@RequestMapping(value = "/contact.html", method = RequestMethod.POST)
+	public void sendComment(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		log.info("UserController: send contact mesage");
+		String subject = request.getParameter("topic");
+		String comment = request.getParameter("message");
+		EmailSender agent = new EmailSender();
+		agent.sendComment(userService.getActiveUser(), subject, comment);
 	}
-
 }
